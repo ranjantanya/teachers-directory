777d913ee6cd5291610206a0c0a232b7af6b0dff
Tanya <tanya@pothi.com>
2020-12-23 20:20:34 +0530

    Fix filtering on subjects and add help text

--- directory/filter.py
+++ directory/filter.py
@@ -10,7 +10,15 @@ __author__ = 'Tanya'
 
 class TeacherFilter(django_filters.FilterSet):
     last_name = django_filters.CharFilter(lookup_expr='istartswith')
-    subjects__name = django_filters.CharFilter(lookup_expr='iexact')
+    subjects__name = django_filters.CharFilter(method='my_custom_filter', label='Subject names', help_text="To search on multiple subjects, enter subject names separated by commas. "
+                                              "e.g. to search a teacher who teaches both physics and biology, enter physics, biology in the box.")
+
     class Meta:
         model = Teacher
         fields = ['last_name', 'subjects__name']
+
+    def my_custom_filter(self, queryset, name, value):
+        subject_list = value.split(',')
+        for subject_name in subject_list:
+            queryset = queryset.filter(subjects__name__icontains=subject_name)
+        return queryset
